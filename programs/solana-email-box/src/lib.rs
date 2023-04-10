3d64067c0c18dcdb2b0d754446cac2a892fe86ad
Heezay <den_9912@mail.ru>
2023-04-10 21:00:32 +0600

    Add accounts

--- programs/solana-email-box/src/lib.rs
+++ programs/solana-email-box/src/lib.rs
@@ -1,4 +1,9 @@
-use anchor_lang::prelude::*;
+use anchor_lang::{prelude::*, InstructionData};
+use solana_program::instruction::Instruction;
+
+const DIRECT_CHAT_SIZE: usize = 1 + 32;
+const MESSAGE_MAX_SIZE: usize = 32 + 1 + 32 + 4 + MAX_STRING_BYTES;
+const MAX_STRING_BYTES: usize = 255;
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
@@ -12,4 +17,40 @@ pub mod solana_email_box {
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+#[instruction(message_seed: Vec<u8>)]
+pub struct SendDirectMessage<'info> {
+    #[account(mut)]
+    pub from: Signer<'info>,
+    pub to: AccountInfo<'info>,
+    #[account(
+    init_if_needed,
+    payer = from,
+    owner = *program_id,
+    seeds = [to.key().as_ref()],
+    bump,
+    space = 8 + DIRECT_CHAT_SIZE
+  )]
+    pub mailbox: Account<'info, Mailbox>,
+    #[account(
+    init_if_needed,
+    payer = from,
+    owner = *program_id,
+    seeds = [message_seed.as_ref()],
+    bump,
+    space = 8 + MESSAGE_MAX_SIZE
+  )]
+    pub message: Account<'info, Message>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct Mailbox {
+    pub inbox: Option<Pubkey>,
+}
+
+#[account]
+pub struct Message {
+    pub from: Pubkey,
+    pub inbox: Option<Pubkey>,
+    pub ciphertext: Vec<u8>,
+}
